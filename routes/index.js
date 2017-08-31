305be157d721f161d795e6cb37a9add6c5096a3f
DESKTOP-3PO0804\Jimmy Liang <kobebryin@gmail.com>
2017-08-31 18:07:19 +0800

    2017.08.31 會員資料頁面UI刻好.

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'TingYi Database Server' });
 });
 
 router.get('/test', function (req, res, next) {
   res.render('test',
     {
-      title: 'Express', 
+      title: 'TingYi Database Server', 
       content: 'Hello World !'
     });
 });
