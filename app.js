52938313a7b469c25756823200590469b2146020
Jimmy <kobebryin@gmail.com>
2017-11-15 15:52:41 +0800

    2017.11.15 將cache打開

--- app.js
+++ app.js
@@ -67,11 +67,11 @@ connection.connect(function (err) {
 });
 /** -------------------------connect MySQL-----------------------END------ */
 
-// // caching disabled for every route
-// app.use(function (req, res, next) {
-//     res.set('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
-//     next();
-// });
+// caching disabled for every route
+app.use(function (req, res, next) {
+    res.set('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
+    next();
+});
 
 //設置session相關設定
 app.use(session({
