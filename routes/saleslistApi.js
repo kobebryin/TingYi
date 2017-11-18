00d3d706026bd6ec9155478ad471760f2f8cafed
DESKTOP-3PO0804\Jimmy Liang <kobebryin@gmail.com>
2017-11-18 23:56:48 +0800

    2017.11.18 會員資料頁面 營養顧問名單與預設設定修改完成 ver.3.0

--- routes/saleslistApi.js
+++ routes/saleslistApi.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 // --------  get Data from MySQL's table member ----------------------- 
 router.get('/', function (req, res, next) {
-    req.dbConnection.query('SELECT ID, Attrib01,Flag FROM member WHERE (Type=0 AND Flag=9) OR (Type=1 AND Flag=9);', function (error, results, fields) {
+    req.dbConnection.query('SELECT ID, Attrib01,Flag FROM member WHERE (Type=0 AND Flag=9) OR (Type=3 AND Flag=9);', function (error, results, fields) {
         if (error) throw error;
         // console.log('The solution is: ', results);
         res.json(results);
